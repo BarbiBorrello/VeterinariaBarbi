e8235634ff367b8246ee5e0d685772462cbc34e2
Barbi Borrello <biborrello@hotmail.com>
2022-06-02 08:53:32 -0300

    algo

---
+++ build/classes/CONTROLADOR/Validaciones_CAMPOS.rs
@@ -0,0 +1,1 @@
+veterinaria_CONTROLADOR.Validaciones_CAMPOS
--- src/CONTROLADOR/VisitaData.java
+++ src/CONTROLADOR/VisitaData.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package CONTROLADOR;
-
+// algo
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
